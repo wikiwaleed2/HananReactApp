f1752b0269dcd10683c9ba54d93f234cf0bf51c2
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-10-17 03:10:10 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- reactApp/src/index.jsx
+++ reactApp/src/index.jsx
@@ -6,6 +6,14 @@ import { history } from './_helpers';
 import { accountService } from './_services';
 import { App } from './app';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { getMainDefinition } from 'apollo-utilities';
+import { ApolloLink, split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import './styles.less';
 
 // setup fake backend
@@ -15,10 +23,42 @@ import './styles.less';
 // attempt silent token refresh before startup
 accountService.refreshToken().finally(startApp);
 
+// GraphQL Initialization
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:5000/graphql',
+});
+ 
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:5000/graphql`,
+    options: {
+      reconnect: true,
+    },
+});
+
+const terminatingLink = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return (
+        kind === 'OperationDefinition' && operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+);
+const link = ApolloLink.from([terminatingLink]);  
+const cache = new InMemoryCache();  
+const client = new ApolloClient({
+    link,
+    cache,
+});
+
+
 function startApp() { 
     render(
         <Router history={history}>
-            <App />
+            <ApolloProvider client={client}>
+                    <App />
+                </ApolloProvider> 
         </Router>,
         document.getElementById('app')
     );
