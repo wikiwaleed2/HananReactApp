28a2acb891d29975cf734d1ef24f0acfb22004e8
Sami Ul Hassan <samihassan0802@gmail.com>
2021-10-21 03:44:15 +0500

    Homepage fixes

--- reactApp/src/_shared/header/index.jsx
+++ reactApp/src/_shared/header/index.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './index.less';
 import logo from '@/_assets/images/dream-maker-logo.svg';
+import aboutIcon from '@/_assets/images/about-dream-makers.svg';
+import settingsIcon from '@/_assets/images/settings-icon.svg';
+import testimonialIcon from '@/_assets/images/testimonial-icon.svg';
+import winnerIcon from '@/_assets/images/winners-icon.svg';
+import crossIcon from '@/_assets/images/cross-icon.svg';
+import chevronBlue from '@/_assets/images/chevron-right-blue.svg';
 import profilePic from '@/_assets/images/blocked-profile.jpg';
 import bellIcon from '@/_assets/images/Bell.svg';
+import appstoreIcon from '@/_assets/images/appstore-icons.png';
+import playStoreIcon from '@/_assets/images/playstore-icons.png';
 import { Link } from "react-router-dom";
 import { getCookie } from '../../_services/cookies.service';
 
 function Header() {
     // const user = accountService.userValue;
     const [user, setUser] = useState(null);
+    const [showNavModal, setShowNavModal] = useState(false);
 
     useEffect(() => {
         // let token =  getCookie("token");
@@ -22,65 +31,105 @@ function Header() {
     }, []);
 
     return (
-        // Navbar
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light customNavbar">
-//             <a className="navbar-brand" href="#">
-//             <img src={logo} alt="Dream Makers" className="d-inline-block align-text-top" />
-//             </a>
-//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//                 <span className="navbar-toggler-icon"></span>
-//             </button>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light bg-gradient customNavbar">
 
-//   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//     <ul className="navbar-nav mr-auto">
-//       <li className="nav-item active">
-//         <a className="nav-link" href="#">How it Works <span className="sr-only">(current)</span></a>
-//       </li>
-//       <li className="nav-item">
-//         <a className="nav-link" href="#">About Dream Makers</a>
-//       </li>
-//       <li className="nav-item">
-//         <a className="nav-link" href="#">Winners</a>
-//       </li>
-//     </ul>
-//     <form className="form-inline my-2 my-lg-0">
-//       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//     </form>
-//   </div>
-// </nav>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light bg-gradient customNavbar">
-            {/* <div className="container-fluid"> */}
-            <a className="navbar-brand" href="#">
-                <img src={logo} alt="Dream Makers" className="d-inline-block align-text-top" />
-            </a>
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Dream Makers" className="d-inline-block align-text-top" />
+                </a>
 
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="#">How it Works</a>
-                    <a className="nav-link" href="#">About Dream Makers</a>
-                    <a className="nav-link" href="#">Winners</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">How it Works</a>
+                        <a className="nav-link" href="#">About Dream Makers</a>
+                        <a className="nav-link" href="#">Winners</a>
+                    </div>
                 </div>
-            </div>
-            <div className="login-btn navbar-nav">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                {!!user ?
-                    <>
-                        <p>Hi {user.firstName}</p>
-                        <img src={profilePic} className="profile-pic" />
-                        <img src={bellIcon} />
-                    </>
-                    : <Link to={{ pathname: `/signup` }}>
-                        <span className="nav-link active" aria-current="page" href="#">Login/Sign-up</span>
-                    </Link>}
+                <div className="login-btn navbar-nav">
+                    {!!user ?
+                        <>
+                            <p>Hi {user.firstName}</p>
+                            <img src={profilePic} className="profile-pic" />
+                            <img src={bellIcon} className="notif-icon" />
+                        </>
+                        :
+                        <>
+                            <button className="navbar-toggler" type="button" onClick={() => setShowNavModal(true)} data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <Link to={{ pathname: `/signup` }}>
+                                <span className="nav-link active" aria-current="page" href="#">Login/Sign-up</span>
+                            </Link>
+                        </>
+                    }
 
 
 
-            </div>
-            {/* </div> */}
-        </nav>
+                </div>
+            </nav>
+
+            {showNavModal && (
+                <div className="modal show m-block" id="navModal">
+                    <div className="modal-dialog modal-md adjust-width" role="document">
+                        <div className="modal-content">
+                            <div className="modal-body">
+
+                                <div className="header-box">
+                                    <p>*New user or existing user<br />Get Started</p>
+                                    <button className="btn btn-primary btn-login">Login/Sign-up</button>
+                                </div>
+
+                                <div className="nav-btns">
+                                    <button className="btn btn-primary btn-page">
+                                        <span>
+                                            <img src={aboutIcon} />
+                                            <p>About Dream Makers</p>
+                                        </span>
+                                        <img src={chevronBlue} />
+                                    </button>
+
+                                    <button className="btn btn-primary btn-page">
+                                        <span>
+                                            <img src={testimonialIcon} />
+                                            <p>Testimonials</p>
+                                        </span>
+                                        <img src={chevronBlue} />
+                                    </button>
+
+                                    <button className="btn btn-primary btn-page">
+                                        <span>
+                                            <img src={winnerIcon} />
+                                            <p>Winners</p>
+                                        </span>
+                                        <img src={chevronBlue} />
+                                    </button>
+
+                                    <button className="btn btn-primary btn-page">
+                                        <span>
+                                            <img src={settingsIcon} />
+                                            <p>How it works</p>
+                                        </span>
+                                        <img src={chevronBlue} />
+                                    </button>
+                                </div>
+
+                                <div className="store-icons">
+                                    <a href="#" >
+                                        <img src={appstoreIcon} />
+                                    </a>
+                                    <a href="#" >
+                                        <img src={playStoreIcon} />
+                                    </a>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </>
     );
 }
 
