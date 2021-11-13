66cfcfee4093793c147f4b3e88c67ac735153ca7
Sami Ul Hassan <samihassan0802@gmail.com>
2021-11-13 18:41:06 +0500

    push

--- reactApp/src/_helpers/fetch-wrapper.js
+++ reactApp/src/_helpers/fetch-wrapper.js
@@ -32,7 +32,7 @@ function put(url, body) {
         headers: { 'Content-Type': 'application/json', ...authHeader(url) },
         body: JSON.stringify(body)
     };
-    return fetch(url, requestOptions).then(handleResponse);    
+    return fetch(url, requestOptions).then(handleResponse);
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
@@ -59,9 +59,10 @@ function authHeader(url) {
 }
 
 function handleResponse(response) {
+    console.log("handle response", response);
     return response.text().then(text => {
         const data = text && JSON.parse(text);
-        
+
         if (!response.ok) {
             if ([401, 403].includes(response.status) && accountService.userValue) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
