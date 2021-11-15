27c4e04885ffcb3e4edfdff585fcdd399d2b982c
Salman Atqiue <salmanxheikh96@gmail.com>
2021-11-16 00:16:05 +0500

    22 issue resolve

--- reactApp/src/_shared/dreamCart/TermsCondition.jsx
+++ reactApp/src/_shared/dreamCart/TermsCondition.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import './dreamCart.css';
 
-const TermsCondition = () => {
+const TermsCondition = ({ checkoutAsGuest, onRadioChange }) => {
    return (
       <>
          <form action="#">
-            <div className="d-flex align-items-center paddingleft-sm">
-               <div className="pt-2">
-                  <input type="radio" checked className="radioBtnSize" />
+            {checkoutAsGuest === 'guest' && (
+               <div className="d-flex align-items-center paddingleft-sm">
+                  <div className="pt-2">
+                     <input type="radio" checked className="radioBtnSize" />
+                  </div>
+                  <div className="ml-2 termsAcceptFont">
+                     I accept the Policy and Terms{' '}
+                     <a href="#" style={{ color: '#104A8E' }}>
+                        Read
+                     </a>
+                  </div>
                </div>
-               <div className="ml-2 termsAcceptFont">
-                  I accept the Policy and Terms{' '}
-                  <a href="#" style={{ color: '#104A8E' }}>
-                     Read
-                  </a>
-               </div>
-            </div>
+            )}
             <div className="d-flex align-items-center mt-3 paddingleft-sm">
                <div className="pt-2">
-                  <input type="radio" checked className="radioBtnSize" />
+                  <input
+                     type="radio"
+                     className="radioBtnSize"
+                     name="checkout-guest"
+                     checked={checkoutAsGuest === 'guest'}
+                     value="guest"
+                     onChange={({ target: { value } }) => {
+                        onRadioChange(value);
+                     }}
+                  />
                </div>
                <div className="ml-2 radioButttonFontSize">
                   Checkout as Guest
@@ -26,7 +37,16 @@ const TermsCondition = () => {
             </div>
             <div className="d-flex align-items-start paddingleft-sm">
                <div className="pt-1">
-                  <input type="radio" className="radioBtnSize" />
+                  <input
+                     type="radio"
+                     className="radioBtnSize"
+                     name="checkout-guest"
+                     checked={checkoutAsGuest === 'notGuest'}
+                     value="notGuest"
+                     onChange={({ target: { value } }) => {
+                        onRadioChange(value);
+                     }}
+                  />
                </div>
                <div className="ml-2 radioButttonFontSize">
                   checkout out with Dream Makers account
