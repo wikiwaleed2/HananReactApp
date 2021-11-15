babe48fb4c351e96f98a64f71dc9293deb396fb9
Salman Atqiue <salmanxheikh96@gmail.com>
2021-11-16 02:03:08 +0500

    hover cart info left

--- reactApp/src/pages/DreamCartInformation/DreamCartInformation.jsx
+++ reactApp/src/pages/DreamCartInformation/DreamCartInformation.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import Card from '../../_shared/dreamCart/Card';
-import { TextField } from '../../_shared/TextField/TextField';
-import { SelectField } from '../../_shared/SelectField/SelectField';
-import CheckoutBtns from '../../_shared/dreamCart/CheckoutBtns';
-import CreditCard from '../../_assets/dreamCart/CreditCard.png';
-import DreamCartSteper from '../../_shared/dreamCart/DreamCartSteper';
-import { NewsLetter } from '../../_shared/newsletter/newsletter';
-import { CountryCodeField } from '../../_shared/CountryCodeField/CountryCodeField';
-import OrderSummery from '../../_shared/dreamCart/OrderSummery';
+import React, { useState, useEffect } from 'react';
 import './DreamCartInformation.css';
+
+import Card from '@/_shared/dreamCart/Card';
+import { TextField } from '@/_shared/TextField/TextField';
+import { SelectField } from '@/_shared/SelectField/SelectField';
+import CheckoutBtns from '@/_shared/dreamCart/CheckoutBtns';
+import CreditCard from '@/_assets/dreamCart/CreditCard.png';
+import DreamCartSteper from '@/_shared/dreamCart/DreamCartSteper';
+import { NewsLetter } from '@/_shared/newsletter/newsletter';
+import { CountryCodeField } from '@/_shared/CountryCodeField/CountryCodeField';
+import OrderSummery from '@/_shared/dreamCart/OrderSummery';
+import CheckoutBilling from '@/_assets/dreamCart/CheckoutBilling.svg';
+import GoogleIcon from '@/_assets/icons/google-sm.png';
+import FacebookIcon from '@/_assets/icons/facebook-sm.png';
+import AppleIcon from '@/_assets/icons/apple-sm.png';
+
 const DreamCartInformation = () => {
+   const [imageShow, setImageShow] = useState(false);
+
+   useEffect(() => {
+      if (window.innerWidth < 576) setImageShow(true);
+      window.addEventListener('resize', () => {
+         if (window.innerWidth < 576) setImageShow(true);
+         else setImageShow(false);
+      });
+
+      return window.removeEventListener('resize', () => {});
+   }, []);
+
    return (
       <>
          <DreamCartSteper />
@@ -18,8 +36,8 @@ const DreamCartInformation = () => {
                <div className="col-12 p-0">
                   <OrderSummery />
                </div>
-               <div className="col-md-12 col-lg-5 py-5 px-3">
-                  <div className="container px-5">
+               <div className="col-md-12 col-lg-5 py-md-5 py-3 px-md-3 px-1">
+                  <div className="container px-md-5 px-4">
                      <div className="row">
                         <div className="col-md-12">
                            <TextField label="First Name" />
@@ -88,14 +106,37 @@ const DreamCartInformation = () => {
                            </div>
                         </div>
                         <div className="col-md-12">
-                           <CheckoutBtns
-                              text="Countinue to Payment"
-                              img={CreditCard}
-                              color="#fff"
-                              backgroundColor="#1663BE"
-                              border="1px solid #707070"
-                           />
+                           <div className="mt-5">
+                              <CheckoutBtns
+                                 text="Countinue to Payment"
+                                 img={imageShow ? CheckoutBilling : CreditCard}
+                                 color="#fff"
+                                 backgroundColor="#1663BE"
+                                 border="1px solid #707070"
+                              />
+                           </div>
                         </div>
+                     </div>
+
+                     <div className="d-flex align-items-center justify-content-center flex-column dream-cart__social-container">
+                        <h6>Sign in with</h6>
+                        <ul className="dream-cart__social">
+                           <li>
+                              <a href="#">
+                                 <img src={GoogleIcon} alt="google" />
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                                 <img src={FacebookIcon} alt="facebook" />
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#">
+                                 <img src={AppleIcon} alt="apple" />
+                              </a>
+                           </li>
+                        </ul>
                      </div>
                   </div>
                </div>
